1f94e6f456bbffeecd3b16b414a5ef4f6404344c
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2022-06-13 18:19:32 -0500

    initilaize project folder structure and scss files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './app.scss';
+import './styles/main.scss';
+import Header from './sections/header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
